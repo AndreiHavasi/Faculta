72a88deb374db13240bf21e45ea435bf684d8cb6
Hornflakes <cristianpetrugug@gmail.com>
2022-08-08 22:18:01 +0300

    In progress: car carousel

--- src/app/home/others/cars/cars.component.ts
+++ src/app/home/others/cars/cars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CarService } from "../../../core/services/car.service";
 import { Car } from "../../../core/classes/car";
 import { Subject, takeUntil } from "rxjs";
@@ -10,8 +10,15 @@ import { Subject, takeUntil } from "rxjs";
 })
 export class CarsComponent implements OnInit {
 
+  @ViewChild('slider') slider!: ElementRef;
+  @ViewChild('sliderContainer') sliderContainer!: ElementRef;
+
   componentDestroyed$: Subject<boolean> = new Subject();
   cars: Car[] = [];
+
+  pressed: boolean = true;
+  startX: number = 0;
+  x: number = 0;
 
   constructor(
     private carService: CarService
@@ -19,6 +26,11 @@ export class CarsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCars();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.sliderContainer);
+    console.log(this.slider);
   }
 
   ngOnDestroy(): void {
@@ -31,6 +43,47 @@ export class CarsComponent implements OnInit {
       .pipe(takeUntil(this.componentDestroyed$))
       .subscribe(cars => this.cars = cars);
   }
+
+  sliderEntered(event: MouseEvent): void {
+    this.sliderContainer.nativeElement.style.cursor = "grab";
+  }
+
+  sliderPressed(event: MouseEvent): void {
+    this.pressed = true;
+    this.startX = event.offsetX - this.slider.nativeElement.offsetLeft;
+    this.sliderContainer.nativeElement.style.cursor = "grabbing";
+  }
+
+  sliderUnPressed(event: MouseEvent): void {
+    this.sliderContainer.nativeElement.style.cursor = "grab";
+    this.pressed = false;
+  }
+
+  sliderGrabbed(event: MouseEvent): void {
+    if (!this.pressed) return;
+    event.preventDefault();
+
+    this.x = event.offsetX;
+    console.log('start X: ' + this.startX);
+    console.log('X: ' + this.x);
+    this.slider.nativeElement.style.left = `${this.x - this.startX}px`;
+
+    this.checkBoundary();
+  }
+
+
+  checkBoundary = () => {
+    let outer =  this.sliderContainer.nativeElement.getBoundingClientRect();
+    let inner =  this.slider.nativeElement.getBoundingClientRect();
+
+    if (parseInt(this.slider.nativeElement.style.left) > 0) {
+      this.slider.nativeElement.style.left = "0px";
+    }
+
+    if (inner.right < outer.right) {
+      this.slider.nativeElement.style.left = `-${inner.width - outer.width}px`;
+    }
+  };
 
 
 }
