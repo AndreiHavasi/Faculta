d1fb3153c130a71a163913c8364041cdbbc4518c
Hornflakes <cristianpetrugug@gmail.com>
2022-08-08 23:27:05 +0300

    Finished: car carousel

--- src/app/home/others/cars/cars.component.ts
+++ src/app/home/others/cars/cars.component.ts
@@ -11,14 +11,9 @@ import { Subject, takeUntil } from "rxjs";
 export class CarsComponent implements OnInit {
 
   @ViewChild('slider') slider!: ElementRef;
-  @ViewChild('sliderContainer') sliderContainer!: ElementRef;
 
   componentDestroyed$: Subject<boolean> = new Subject();
   cars: Car[] = [];
-
-  pressed: boolean = true;
-  startX: number = 0;
-  x: number = 0;
 
   constructor(
     private carService: CarService
@@ -26,11 +21,6 @@ export class CarsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCars();
-  }
-
-  ngAfterViewInit(): void {
-    console.log(this.sliderContainer);
-    console.log(this.slider);
   }
 
   ngOnDestroy(): void {
@@ -44,46 +34,36 @@ export class CarsComponent implements OnInit {
       .subscribe(cars => this.cars = cars);
   }
 
-  sliderEntered(event: MouseEvent): void {
-    this.sliderContainer.nativeElement.style.cursor = "grab";
+  /** DRAGGABLE CAROUSEL **/
+
+  grabbed = false;
+  initialX: any;
+  scrollLeft: any;
+
+  mousedown(e: MouseEvent): void {
+    this.grabbed = true;
+    this.slider.nativeElement.classList.add('active');
+    this.initialX = e.pageX - this.slider.nativeElement.offsetLeft;
+    this.scrollLeft = this.slider.nativeElement.scrollLeft;
   }
 
-  sliderPressed(event: MouseEvent): void {
-    this.pressed = true;
-    this.startX = event.offsetX - this.slider.nativeElement.offsetLeft;
-    this.sliderContainer.nativeElement.style.cursor = "grabbing";
+  mouseleave(): void {
+    this.grabbed = false;
+    this.slider.nativeElement.classList.remove('active');
   }
 
-  sliderUnPressed(event: MouseEvent): void {
-    this.sliderContainer.nativeElement.style.cursor = "grab";
-    this.pressed = false;
+  mouseup(): void {
+    this.grabbed = false;
+    this.slider.nativeElement.classList.remove('active');
   }
 
-  sliderGrabbed(event: MouseEvent): void {
-    if (!this.pressed) return;
-    event.preventDefault();
+  mousemove(e: MouseEvent): void {
+    if(!this.grabbed) return;
+    e.preventDefault();
 
-    this.x = event.offsetX;
-    console.log('start X: ' + this.startX);
-    console.log('X: ' + this.x);
-    this.slider.nativeElement.style.left = `${this.x - this.startX}px`;
-
-    this.checkBoundary();
+    const x = e.pageX - this.slider.nativeElement.offsetLeft;
+    const dx = (x - this.initialX);
+    this.slider.nativeElement.scrollLeft = this.scrollLeft - dx;
   }
-
-
-  checkBoundary = () => {
-    let outer =  this.sliderContainer.nativeElement.getBoundingClientRect();
-    let inner =  this.slider.nativeElement.getBoundingClientRect();
-
-    if (parseInt(this.slider.nativeElement.style.left) > 0) {
-      this.slider.nativeElement.style.left = "0px";
-    }
-
-    if (inner.right < outer.right) {
-      this.slider.nativeElement.style.left = `-${inner.width - outer.width}px`;
-    }
-  };
-
 
 }
